4c7a3575d6beffc1f53d04c3202a6da543c5ca41
ckahn <ckahn@deleo.co.kr>
2024-11-15 20:59:28 +0900

    SecurityUtil 내 관리자 정보 check 메소드 추가

--- src/main/java/co/kr/common/util/SecurityUtil.java
+++ src/main/java/co/kr/common/util/SecurityUtil.java
@@ -1,5 +1,6 @@
 package co.kr.common.util;
 
+import co.kr.common.code.UserType;
 import co.kr.common.exception.InvalidTokenException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -7,14 +8,28 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 
 public class SecurityUtil {
-    public static String getUserId() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (!(authentication instanceof UsernamePasswordAuthenticationToken)) {
-            throw new InvalidTokenException("알 수 없는 인증 정보");
-        }
+	public static String getUserId() {
+		Authentication authentication = getAuthentication();
 
-        User user = (User) authentication.getPrincipal();
-        return user.getUsername();
-    }
+		User user = (User) authentication.getPrincipal();
+		return user.getUsername();
+	}
+
+	public static boolean isAdmin() {
+		Authentication authentication = getAuthentication();
+
+		return authentication.getAuthorities().stream()
+			.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(UserType.SYS_ADMIN.getCode()));
+	}
+
+	private static Authentication getAuthentication() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (!(authentication instanceof UsernamePasswordAuthenticationToken)) {
+			throw new InvalidTokenException("알 수 없는 인증 정보");
+		}
+
+		return authentication;
+	}
 }

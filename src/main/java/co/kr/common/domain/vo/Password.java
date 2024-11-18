55e9e16183389aa30c95a1ecfb8b9d88eeb6851a
ckahn <ckahn@deleo.co.kr>
2024-11-18 09:50:21 +0900

    불필요 코드 정리

--- src/main/java/co/kr/common/domain/vo/Password.java
+++ src/main/java/co/kr/common/domain/vo/Password.java
@@ -8,8 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.Value;
 import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Value
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -27,24 +25,4 @@ public class Password {
 	 public static Password newInstanceWithEncrypted(@NonNull String value) {
 		return newInstance(passwordEncoder().encode(value));
 	 }
-
-	public static void main(String[] args) {
-	        String rawPassword = "test1234";
-	        String encodedPassword = "$2a$10$7USoea5CtD6iA/w5e9uhO.g5hK65EArbFXygJq9sry2vmLtiGOgLu";
-
-	        // BCryptPasswordEncoder 생성
-	        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-	        // {bcrypt} 접두사를 제거 (Spring Security는 자동으로 처리하기도 함)
-	        encodedPassword = encodedPassword.replace("{bcrypt}", "");
-
-	        // 비밀번호 비교
-	        boolean isMatch = passwordEncoder.matches(rawPassword, encodedPassword);
-
-	        if (isMatch) {
-	            System.out.println("비밀번호가 일치합니다!");
-	        } else {
-	            System.out.println("비밀번호가 일치하지 않습니다.");
-	        }
-	    }
 }
